ba1915e6646fd2c43ca95c6a938a197a0a3c5a15
Tom <tom.limpens@hotmail.com>
2020-06-04 10:57:39 +0200

    new repo

---
+++ HourRegistrationService/Controllers/HourController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HourRegistrationService.Controllers
+{
+
+    [Route("hours")]
+    public class HourController : Controller
+    {
+
+        [HttpGet("GetHours")]
+        public string get()
+        {
+            return "This is the Hour registration service service";
+        }
+
+        [HttpGet("getMontlyWorkedHoursForUser/{userId}")]
+        public int getMontlyWorkedHoursForUser(int userId)
+        {
+            return 28;
+        }
+
+        [HttpGet("getMontlyScheduledHoursForUser/{userId}")]
+        public int getMontlyScheduledHoursForUser(int userId)
+        {
+            return 35;
+        }
+
+    }
+}
